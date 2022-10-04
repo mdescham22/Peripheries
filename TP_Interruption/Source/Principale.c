1f700fcb13568b1ad483d5b08f6f57a79eb5d1ba
Aishya Costa <acosta@insa-toulouse.fr>
2022-10-04 18:12:46 +0200

    Test_branche

---
+++ TP_Interruption/Source/Principale.c
@@ -0,0 +1,14 @@
+#include "Driver_GPIO.h"
+#include <stdlib.h>
+
+//-------------- Reponses aux questions --------------
+// 1. Il faut agir sur les bits de 0 a 7  au niveau du registre TIM_DIER pour
+//     activer les interruptions.
+//2.  Enabler 1 des bits des registres ISER0-ISER2 pour activer interruptions du NVIC
+//3.  Lorsqu'il y a débordement 
+
+int main(void)
+{
+
+	while(1);
+}
